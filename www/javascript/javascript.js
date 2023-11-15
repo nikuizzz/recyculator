7bcc334a5cd06c8bb640f99ce8c73a668fab40dd
nikuizzz <nikitazuz@gmail.com>
2023-11-15 11:35:04 +0100

    frontend tweaks

--- www/javascript/javascript.js
+++ www/javascript/javascript.js
@@ -1,71 +1,14 @@
-var current_bg = ""
-var next_bg = ""
+// Import variables and functions from recycler
+// I have to put "export" before the variable/function inside of 
+// recycler.js so it can be imported there
+import { rItems, rKeys, recycle, recycleAll} from "./recycler.js"
 
-var current_bg_cont = document.getElementById("current-bg")
-var next_bg_cont = document.getElementById("next-bg")
+const allItemsContainer = document.getElementById("all-items-container")
+const inputItemsContainer = document.getElementById("input-items-container")
+const outputItemsContainer = document.getElementById("output-items-container")
 
-let backgrounds = {
-    "Arctic Labs": "arcticlabs.png",
-    "Mountain" : "mountain.png",
-    "Boat Village": "boatvillage1.png",
-    "Scrap Heli": "scrapheli.png",
-    "Desert": "desert.png",
-    "Forest": "forest.png",
-    "Outpost": "outpost.png",
-    "Supermarket": "supermarket.png",
-    "Swamp": "swamp.png",
-    "Underwater Labs" : "underwaterlabs.png",
-}
-
-function cycleBackground(next_bg) {
-    // First call
-    if(next_bg === "") {
-        current_bg = getRandomBackground(current_bg)
-        setBackground(getPath(current_bg))
-    }
-    // Cycle
-    else {
-        // Updating next background
-        setNextBackground(getPath(next_bg))
-
-        // Fading out current background
-        current_bg_cont.style.opacity = "0"
-
-        // Fading in next background
-        next_bg_cont.style.opacity = "1"
-
-        // Permutation of objects 
-        let temp = current_bg_cont
-        current_bg_cont = next_bg_cont
-        next_bg_cont = temp
-    }
+// console.log(recycle("Rope", 5))
 
 
-    next_bg = getRandomBackground()
+// Adding elements to ITEMS section
 
-    setTimeout(() => {
-        cycleBackground(next_bg)
-    }, 15000)
-}
-
-function getRandomBackground(background) {
-    // Returns a random background, different from the background given in parameters
-    let bg = Object.keys(backgrounds)[Math.floor(Math.random() * Object.keys(backgrounds).length)]
-    
-    while(bg === background) {
-        bg = Object.keys(backgrounds)[Math.floor(Math.random() * Object.keys(backgrounds).length)]
-    }
-    
-    return bg
-}
-function getPath(bg) {
-    return `./misc/backgrounds/${backgrounds[bg]}`
-}
-function setBackground(path) {
-    current_bg_cont.style.backgroundImage = `url("${path}")`
-}
-function setNextBackground(path) {
-    next_bg_cont.style.backgroundImage = `url("${path}")`
-}
-
-setBackground(getPath(Object.keys(backgrounds)[1]))
--- www/javascript/recycler.js
+++ www/javascript/recycler.js
@@ -1,15 +1,15 @@
 import data from "./recycling.json" assert { type: 'json' }
 
-var rItems = data
-var rKeys = Object.keys(data)
+export var rItems = data
+export var rKeys = Object.keys(data)
 
 // console.log(rItems)
 // console.log(rKeys)
 
-function sumItems(items) {
-    itemsToDelete = []
-
+// Delete??
+export function sumItems(items) {
     // Searching for duplicated ressources, summing amounts
+    console.log(items)
     for(let i = 0; i < items.length; i++) {
         let name = items[i][0]
 
@@ -22,25 +22,23 @@ function sumItems(items) {
                 // Each duplicated item in items array is set to ["", 0]
                 // so we can remove duplicates easily after
                 items[j+1] = ["", 0]
-
-                itemsToDelete.push(j+1)
             }
         }
     }
 
+    // console.log(items)
     // Removing duplicates with Set
-    items = Array.from(new Set(items))
+    // items = Array.from(new Set(items))
 
-
-    // Removing last ["", 0]
-    items.pop(items.findIndex( (element) => {
-        return element == ["", 0]
-    }))
+    // // Removing last ["", 0]
+    // items.pop(items.findIndex( (element) => {
+    //     return element == ["", 0]
+    // }))
 
     return items
 }
 
-function recycle(resource, amount) {
+export function recycle(resource, amount) {
     let result = {}
     let item = resource
 
@@ -69,7 +67,7 @@ function recycle(resource, amount) {
     }
 }
 
-function recycleAll(items, fullRecycle) {
+export function recycleAll(items, fullRecycle) {
     // items -> { "Cloth": 10, "Rope": 3}...
     let result = {}
 
@@ -105,7 +103,7 @@ function recycleAll(items, fullRecycle) {
         }
     }
 
-    return sumItems(result)
+    return result
 }
 
-console.log(recycleAll({"Sewing Kit": 1, "Gears": 10, "Rope": 5, "Assault Rifle": 2, "Auto Turret": 3}, false))
+console.log(recycleAll({"Sewing Kit": 3, "Rope": 10, "Tarp": 3, "Gears": 3, "Electric Fuse": 4}, true))
