003586dde3e70753603ff68a0a58e2d1a80cb270
nikuizzz <nikitazuz@gmail.com>
2023-11-16 14:33:33 +0100

    modified scraper.py so it also scraps the raw resources we might get from recycling

--- imgscaler.py
+++ imgscaler.py
@@ -1,12 +1,16 @@
 import os
 from PIL import Image
 
-input_folder_path = "./www/misc/backgrounds/"
-output_folder_path = "./www/misc/backgrounds/"
+# input_folder_path = "./www/misc/backgrounds/"
+# output_folder_path = "./www/misc/backgrounds/"
+
+input_folder_path = "./www/misc/items180/"
+output_folder_path = "./www/misc/items80/"
 
 image_paths = os.listdir(input_folder_path)
 
-size = 1920, 1080
+# size = 1920, 1080
+size = 80, 80
 
 for path in image_paths:
     original_image = Image.open(input_folder_path + path)
--- scraper.py
+++ scraper.py
@@ -14,18 +14,55 @@ if not resp.ok:
 bs = BeautifulSoup(resp.text, 'html.parser')
 items = {}
 
+# Cycling through each table row to get every recyclable item name
 for cell in bs.findAll('tr')[1::]:
     links = cell.findAll("a")
-
     path = cell.find("img")['src'].split("/")[-1]
 
     # Getting the name of the item
     name = links[0].text
 
+    isRecyclable = True
+    
+    category = cell.findAll("td")[-1].text
+
+    item = {
+        "name": name,
+        "recyclable": isRecyclable,
+        "link": links,
+        "path": path,
+    }
+
+    items[name] = item
+
+for key in list(items.keys()).copy():
+    element = items[key]
+    name = element["name"] 
+    isRecyclable = element["recyclable"] 
+    path = element["path"] 
+    links = element["link"]
+
+    # Cycling through each recycle result
     # Getting recycle results and chances 
     resources = {}
     for link in links[1::]:
         resource_name = link.find("img")['title']
+
+        # Adding the resource in the items dict if it is not already inside of it
+        # It allows to get every raw material that we can get from recycling items
+        if resource_name not in items.keys():
+            path = link.find("img")['src'].split("/")[-1]
+
+            item = {
+                "name": resource_name,
+                "recyclable": False,
+                "resources": {},
+                "category": "Resource",
+                "path": path,
+            }
+
+            items[resource_name] = item
+
         resource_count = link.find("span", {"class": "text-in-icon"}).text
         probability = "100"
 
@@ -57,6 +94,7 @@ for cell in bs.findAll('tr')[1::]:
     }
 
     items[name] = item
+
 
 with open("./www/javascript/recycling.json", "w") as file: 
     json.dump(items, file, indent=4)
